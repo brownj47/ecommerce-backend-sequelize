a104ab396d7ee0f347b97858d9b8a6e1ff7ae9de
Justus B <justus.c.brown@gmail.com>
2022-07-31 15:09:08 -0700

    category routes

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,24 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    product_name: {
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type:DataTypes.DECIMAL,
+      allowNull:false,
+      validate:{isDecimal: true}
+    },
+    stock: {
+      type:DataTypes.INTEGER,
+      allowNull:false,
+      defaultValue: 10, 
+      validate:{isInt:true}
+    },
+    // category_id: {
+    //   type:DataTypes.INTEGER,
+    // }
   },
   {
     sequelize,
